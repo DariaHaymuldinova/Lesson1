b4aa9a38f97a62456d9b8a6386bfe5d13347a529
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-09 12:38:35 +0400

    Добавили задачу 8

---
+++ задача8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа: показывает четные числа от 1 до N
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int k = 1;
+while (true)
+{
+if (k % 2 == 0)
+{
+    Console.WriteLine(k + "");
+}
+k++;
+if (k > number)
+{
+    break;
+}
+}
+Console.ReadLine();
