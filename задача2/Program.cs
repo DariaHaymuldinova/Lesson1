208bebe6c3229b1c324a798d4071cd6dc1b3662a
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-09 12:27:56 +0400

    Добавили задачу 2

---
+++ задача2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа: находит большее число из двух чисел
+
+Console.WriteLine("Введите два числа: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine("{0} наибольшее число", numberA);
+}
+else
+{
+    Console.WriteLine("{0} наибольшее число", numberB);
+}
+Console.ReadLine();
