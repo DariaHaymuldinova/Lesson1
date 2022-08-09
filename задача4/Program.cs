eeaab816d0d36cd2ef3928540278b420622277f6
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-09 12:33:20 +0400

    Добавили задачу 4

---
+++ задача4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа: находит большее число из трех чисел
+
+Console.WriteLine("Введите три числа: ");
+int numberA = Convert.ToInt32(Console.ReadLine()); 
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+if ((numberA > numberB) && (numberA > numberC))
+{
+    Console.WriteLine("{0} наибольшее число", numberA);
+}
+else if ((numberA < numberB) && (numberB > numberC))
+{
+    Console.WriteLine("{0} наибольшее число", numberB);
+}
+else
+{
+    Console.WriteLine("{0} наибольшее число", numberC);
+}
+Console.ReadLine();
